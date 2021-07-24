326fc68ac5f24b75c70abb6bc1038d75981ff5a2
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-23 22:33:45 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        String[] arr = {"John", "Michelle", "Tommy"};
+//        String[] arr2 = new String[5];
+
+//        System.out.println(arr.length);
+//        List<String> list = new ArrayList<>();
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        arrayList.add(0, 9);
+//        arrayList.add(5);
+//        arrayList.add(7);
+//        arrayList.add(9);
+//        System.out.println(arrayList.get(1));
+//        System.out.println(arrayList.size());
+//        Collections.reverse(arrayList);
+//        System.out.println(arrayList.toString());
+
+//        String myString = "I really like ice cream";
+//        String[] str = myString.split(" ");
+//        System.out.println(Arrays.toString(str));
+//        List<String> arrayString = new ArrayList<>();
+//        arrayString = Arrays.asList(str);
+//        System.out.println(arrayString.toString());
+
+//        HashMap<Integer, String> map = new HashMap<>();
+//        map.put(1, "Pizza");
+//        map.put(2, "Pineapple");
+//        map.put(3, "Borger");
+//        map.remove(3);
+//        map.put(1, "Chicken");
+//        System.out.println(map.get(1));
+//        System.out.println(map.containsKey(1));
+//        System.out.println(map.containsValue("Pizza"));
+
+//        ArrayList<String> carArray = new ArrayList<> ();
+//        carArray.add("Honda");
+//        carArray.add("Toyota");
+//        carArray.add("Ford");
+//        for(String car: carArray) {
+//            System.out.println(car);
+//        }
+
+//        HashMap<Integer, String> map = new HashMap<>();
+//        for(Map.Entry<Integer, String> entry : map.entrySet()) {
+//            int key = entry.getKey();
+//            String value = entry.getValue();
+//            System.out.println(key + " " + value);
+//        }
+
+// Exercise 1 (forEach): Ask the user for 5 numbers and store them in an array list.
+// Then find the sum, product, largest, and smallest of those numbers.
+
+
+
+
+// Exercise 2 (Map.Entry): You are a car dealer - create a hash map of vehicles:
+// The model is the Key, the make is the Value.
+// Ask the customer what car (model) they are looking for,
+// use the HashMap to determine if you have that vehicle, and what make it is.
+// (e.g., "Oh, you're looking for a Civic? Our Honda selection is right over here...")
+
+//        HashMap<Integer, String> map = new HashMap<>();
+//        map.put(1, "Pizza");
+//        map.put(2, "Pineapple");
+//        map.put(3, "Borger");
+//        map.remove(3);
+//        map.put(1, "Chicken");
+//        System.out.println(map.get(1));
+//        System.out.println(map.containsKey(1));
+//        System.out.println(map.containsValue("Pizza"));
+
+//        ArrayList<String> carArray = new ArrayList<> ();
+//        carArray.add("Honda");
+//        carArray.add("Toyota");
+//        carArray.add("Ford");
+//        for(String car: carArray) {
+//            System.out.println(car);
+//        }
+
+
+
+
+
+    }
+}
